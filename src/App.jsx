7803151be9c3a823aa20fb4da08d21df4cffba37
Raven <raven.guirao@gmail.com>
2024-11-14 16:58:24 +0800

    exam answers

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import "./App.css";
+import Spinner from "./components/Spinner/Spinner";
+import Card from "./components/Card/Card";
+import { useState, useEffect, useRef, useCallback } from "react";
+
+function App() {
+  const [launches, setLaunch] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [offset, setOffset] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    setOffset(0);
+    setLaunch([]);
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    const query = `rocket_name=${search}`;
+    fetch(
+      `https://api.spacexdata.com/v3/launches/?${
+        query + "&"
+      }limit=10&offset=${offset}`
+    )
+      .then((res) => res.json())
+      .then((launches) => {
+        if (launches.length < 10) {
+          setHasMore(false);
+        }
+        setLaunch((prevState) => [...prevState, ...launches]);
+        setLoading(false);
+        console.log(offset, hasMore);
+      });
+  }, [offset, hasMore, search]);
+
+  const observer = useRef();
+  const lastLaunchElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setOffset((prevOffset) => prevOffset + 10);
+          console.log("intersecting");
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
+
+  console.log(launches);
+  return (
+    <div className="App">
+      <input
+        value={search}
+        onChange={handleSearch}
+        className="search"
+        type="text"
+        placeholder="Search Mission Name"
+      ></input>
+      <div className="list">
+        {launches.map((launch, index) => {
+          if (launches.length === index + 1) {
+            return (
+              <Card
+                lastRef={lastLaunchElementRef}
+                images={launch.links.flickr_images}
+                key={launch.flight_number}
+                header={launch.mission_name}
+                badge={launch.upcoming}
+                year={launch.launch_date_unix}
+              />
+            );
+          } else {
+            return (
+              <Card
+                imagesLink={launch.links.flickr_images}
+                key={launch.flight_number}
+                header={launch.mission_name}
+                badge={launch.upcoming}
+                year={launch.launch_date_unix}
+              />
+            );
+          }
+        })}
+        {loading && <Spinner />}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./card.scss";
+
+export default function Card({ lastRef, header, badge, year, imagesLink }) {
+  const [showContent, setShowContent] = useState(false);
+  const images =
+    imagesLink && imagesLink.length > 0 ? (
+      imagesLink.map((imageLink) => (
+        <img alt="mission images" src={imageLink} />
+      ))
+    ) : (
+      <p>"No images"</p>
+    );
+    
+  return (
+    <div ref={lastRef} className="card">
+      <div className="card-header">
+        <h1>{header}</h1>
+        <div>
+          <p className={`badge ${badge ? "blue" : "green"}`}>
+            {badge ? "upcoming" : "done"}
+          </p>
+        </div>
+      </div>
+      <div className="card-body">
+        <p className="date">in year {new Date(year * 1000).getFullYear()}</p>
+        {/* map image here */}
+        {showContent && images}
+      </div>
+      <button className="btn" onClick={() => setShowContent(!showContent)}>
+        {showContent ? "HIDE" : "VIEW"}
+      </button>
+    </div>
+  );
+}
